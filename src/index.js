5d310750b080aed49c06edbf8c226179fcb475f3
Krystian Laskowski <krystian.laskowski@valentinite.io>
2017-09-26 23:31:14 +0200

    Purely webcomponent way to inject the component. Style adjustments.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import Feed from './Feed'
 import './index.css'
 import './ApiMock'
 
-const feeds = document.getElementsByTagName('feed');
-for(let i=0; i<feeds.length; i++) render(
-  <Feed
-    uuid={feeds[i].attributes["data-uuid"]}
-    orientation={feeds[i].attributes["vertical"] ? "vertical" : "horizontal"}
-  />, feeds[i]
-)
+// const feeds = document.getElementsByTagName('feed');
+// for(let i=0; i<feeds.length; i++) render(
+//   <Feed
+//     uuid={feeds[i].attributes["data-uuid"]}
+//     orientation={feeds[i].attributes["vertical"] ? "vertical" : "horizontal"}
+//   />, feeds[i]
+// )
+
+const proto = Object.create(HTMLElement.prototype, {
+  attachedCallback: {
+    value: function() {
+      const mountPoint = document.createElement('div');
+      this.createShadowRoot().appendChild(mountPoint);
+      render(<Feed uuid={this.getAttribute("data-uuid")}/>, mountPoint);
+    }
+  }
+});
+document.registerElement('x-feed', {prototype: proto});

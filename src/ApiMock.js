e601a3d97079e3aefd6f7e931a7a5a8139a840d2
Krystian Laskowski <krystian.laskowski@valentinite.io>
2017-09-27 17:16:25 +0200

    Pub/sub mechanism

--- src/ApiMock.js
+++ src/ApiMock.js
@@ -1,4 +1,33 @@
+import Channel from './Event'
+
 window.AllexisSDK = {
+    channel: new Channel(),
+    playedItem: null,
     getFeed: feedId => fetch("https://api.myjson.com/bins/m0znl").then(response => response.json()),
-    getPriceForProduct: productId => new Promise(resolve => resolve("R 20,00"))
+    getPriceForProduct: productId => new Promise(resolve => resolve("R 20,00")),
 }
+
+window.AllexisSDK.channel.subscribe("play", id => {
+    if(id !== window.AllexisSDK.playedItem) {
+        if(window.AllexisSDK.playedItem != null) {
+            console.log(`Channel stopped playing ${id}`)
+            window.AllexisSDK.channel.publish("player:stopped", window.AllexisSDK.playedItem)
+        }
+        window.AllexisSDK.playedItem = id
+        console.log(`Channel started playing ${id}`)
+        window.AllexisSDK.channel.publish("player:started", id)
+    }
+})
+
+window.AllexisSDK.channel.subscribe("buy", id => {
+    console.log(`Channel received a call to buy ${id}`)
+})
+
+window.AllexisSDK.channel.subscribe("stop", id => {
+    if(id === window.AllexisSDK.playedItem
+    && window.AllexisSDK.playedItem != null) {
+        window.AllexisSDK.playedItem = null
+        console.log(`Channel stopped playing ${id}`)
+        window.AllexisSDK.channel.publish("player:stopped", id)
+    }
+})
--- src/Event.js
+++ src/Event.js
@@ -1,42 +1,18 @@
-export default function trigger(event, ...params) {
-  console.log(`Trigger ${event} with ${JSON.stringify(params)}`)
-}
-
-export function request(name, ...params) {
-  console.log(`Requested ${name} with ${JSON.stringify(params)}`)
-}
-
-export function reply(name, ...params) {
-  console.log(`SDK responded to ${name} with ${JSON.stringify(params)}`)
-}
-
-export function stopReplyingTo(name, fn) {
-  console.log(`Stop responding to ${name}${fn.name ? ' with ' + fn.name : ''}`)
-}
-
-export function listenTo(event, ...params) {
-  console.log(`SDK received ${event} with ${JSON.stringify(params)}`)
-}
-
-export function stopListeningTo(event, fn) {
-  console.log(`Stop listening to ${event}${fn.name ? ' with ' + fn.name : ''}`)
-}
-
-export class Channel {
+export default class Channel {
   constructor () {
     this.events = {}
   }
   publish(name, ...params) {
     console.log(`Published ${name} with ${JSON.stringify(params)}`)
-    if(this.events.name instanceof Array) this.events.name.forEach(fn => fn(...params))
+    if(this.events[name] instanceof Array) this.events[name].forEach(listener => listener.fn.apply(listener.scope, params))
   }
-  subscribe(name, fn) {
-    if(this.events.name instanceof Array) this.events.name.push(fn)
-    else this.events.name = [fn]
+  subscribe(name, fn, scope) {
+    if(this.events[name] instanceof Array) this.events[name].push({fn, scope})
+    else this.events[name] = [{fn, scope}]
     console.log(`Subscribed to ${name} with ${fn.name}`)
   }
   unsubscribe(name, removedFn) {
-    if(this.events.name instanceof Array) this.events.name = this.events.name.filter(fn => fn !== removedFn)
+    if(this.events[name] instanceof Array) this.events[name] = this.events[name].filter(fn => fn !== removedFn)
     console.log(`Unsubscribed from ${name} with ${removedFn.name}`)
   }
 }
